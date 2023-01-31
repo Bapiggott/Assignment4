9e7ce9db4a48496c50a9c89dcb07bdd0f17e3eeb
Brett Piggott <bapiggott@oakland.edu>
2023-01-31 12:29:28 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+
+
+public class App extends SuperClass{
+    public static void main(String[] args) throws Exception {
+    App testObject = new App();
+    testObject.print();
+    System.out.println("Hello, World!");
+    }
+}
